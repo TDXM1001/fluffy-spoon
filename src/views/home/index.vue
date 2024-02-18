1ff3af5f885521069225fd0e6a0e986608bab676
TDXM1001 <2678062447@qq.com>
2024-02-18 11:57:35 +0800

    调整首页布局(待完善)

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,6 +1,22 @@
 <template>
-  <div class="home card">
-    <img class="home-bg" src="@/assets/images/welcome.png" alt="welcome" />
+  <div>
+    <div class="grid grid-cols-4 gap-5">
+      <div class="bg-white rounded-md h-28 border-neutral-400"></div>
+      <div class="bg-white rounded-md h-28 border-neutral-400"></div>
+      <div class="bg-white rounded-md h-28 border-neutral-400"></div>
+      <div class="bg-white rounded-md h-28 border-neutral-400"></div>
+    </div>
+
+    <div class="grid grid-flow-col grid-rows-3 gap-5 my-5 h-96">
+      <div class="col-span-2 row-span-3 bg-white rounded-md"></div>
+      <div class="col-span-2 bg-white rounded-md"></div>
+      <div class="col-span-2 row-span-2 bg-white rounded-md"></div>
+    </div>
+
+    <div class="grid grid-flow-col grid-rows-2 gap-5 my-5 h-96">
+      <div class="row-span-2 bg-white rounded-md"></div>
+      <div class="row-span-2 bg-white rounded-md"></div>
+    </div>
   </div>
 </template>
 
